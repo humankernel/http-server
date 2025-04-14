176d586f374de16604448e3267d8d28194e9dd92
strange <rivasjoaquin02@gmail.com>
2025-04-13 20:59:21 -0400

    stage 2

--- app/main.go
+++ app/main.go
@@ -7,18 +7,21 @@ import (
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	listener, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	defer listener.Close()
 
-	_, err = l.Accept()
+	fmt.Println("Server listening on port 4221")
+
+	conn, err := listener.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	fmt.Println("sending msg to client..")
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
